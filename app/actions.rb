d57c463629dce73524216ea529a4e6a8b9b063fb
brandon <blee298@gmail.com>
2014-10-22 23:36:46 +0000

    adding views and actions

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,46 @@
+enable :sessions
+
+helpers do 
+
+  def current_player
+    return Player.find(session[:id]) if session[:id]
+  end
+
+end  
+
 # Homepage (Root path)
 get '/' do
   erb :index
 end
+
+###SIGN UP
+get '/signup' do 
+  erb :'players/signup'
+end 
+
+post '/signup' do 
+  @player = Player.new(
+    email: params[:email],
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    password: params[:password])
+  erb :'players/signup'
+end 
+
+###LOGIN 
+get '/login' do
+  erb :'players/login'
+end
+
+post '/login' do
+  @player = Player.find_by email:(params:[:email])
+  session[:id] = @player.id
+end  
+
+###LOGOUT 
+get '/logout' do 
+  session.clear
+  redirect '/login'
+end 
+
+###
